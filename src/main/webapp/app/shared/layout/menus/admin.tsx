b7f026a62b621cd68129a2322da86cbbb3d52b62
eduardojc-met <104899902+eduardojc-met@users.noreply.github.com>
2022-06-01 15:08:59 +0200

    xyz

--- src/main/webapp/app/shared/layout/menus/admin.tsx
+++ src/main/webapp/app/shared/layout/menus/admin.tsx
@@ -9,6 +9,18 @@ const adminMenuItems = (
     <MenuItem icon="users" to="/admin/user-management">
       User management
     </MenuItem>
+    <MenuItem icon="tachometer-alt" to="/admin/metrics">
+      Metrics
+    </MenuItem>
+    <MenuItem icon="heart" to="/admin/health">
+      Health
+    </MenuItem>
+    <MenuItem icon="cogs" to="/admin/configuration">
+      Configuration
+    </MenuItem>
+    <MenuItem icon="tasks" to="/admin/logs">
+      Logs
+    </MenuItem>
     {/* jhipster-needle-add-element-to-admin-menu - JHipster will add entities to the admin menu here */}
   </>
 );
